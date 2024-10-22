022b10e5594bbe3af0f327f022791ba44423a088
Muhammad Abid <abidroid1@gmail.com>
2024-10-22 18:37:22 +0500

    simple calculator

---
+++ day3/simple_calculator.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+message = """
+Select Operation:
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+Your choice: ? """
