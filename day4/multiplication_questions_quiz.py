c88f3ebe38e5eb4310956e0e3b7180cfa05d86a5
Muhammad Abid <abidroid1@gmail.com>
2024-10-28 10:15:55 +0500

    fixes

--- day4/multiplication_questions_quiz.py
+++ day4/multiplication_questions_quiz.py
@@ -1,8 +1,9 @@
 import random
 
 count = 1
+total_correct = 0
 
-while count <= 4:
+while count <= 12:
     num1 = random.randint(2, 9)
     num2 = random.randint(2, 9)
     correct_answer = num1 * num2
@@ -10,8 +11,11 @@ while count <= 4:
     user_answer = int(input(f"{num1} x {num2} = "))
     if user_answer == correct_answer:
         print("Correct!")
+        total_correct += 1
     else:
         print("Incorrect!")
         print("Correct answer:", correct_answer)
 
     count += 1
+
+print(f"Total number of correct answers: {total_correct}")
