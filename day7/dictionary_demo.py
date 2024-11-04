9bce4459c6918d8e4dfbd2c57d73048d5cca8405
Muhammad Abid <abidroid1@gmail.com>
2024-11-04 21:23:56 +0500

    dictionary in python

---
+++ day7/dictionary_demo.py
@@ -0,0 +1,9 @@
+players_jersey = {}
+
+student = {
+    "name": "Ali",
+    "uni": "KIU",
+    "age": 21,
+    "grade": "A"
+}
+
--- day7/hangman_game.py
+++ day7/hangman_game.py
@@ -5,3 +5,5 @@ word_list = ["apple", "banana", "cherry", "table", "fruit", "bed", "peshawar"]
 chosen_word = random.choice(word_list)
 
 print(chosen_word)
+
+guess = input("Guess a letter: ").lower()
