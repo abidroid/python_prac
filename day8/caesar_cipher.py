85a6698b1c0703634d1deee2aa8025a6e66cece0
Muhammad Abid <abidroid1@gmail.com>
2024-11-10 21:11:20 +0500

    decrypt

--- day8/caesar_cipher.py
+++ day8/caesar_cipher.py
@@ -15,11 +15,28 @@ def encrypt(original_text, shift_num):
     print(generated_text)
         
 
+def decrypt( original_text, shift_num):
+    generated_text = ""
+
+    for character in original_text:
+        index = alphabet.index(character)
+        index_with_shift = index - shift_num
+
+        if( index_with_shift < 0):
+            difference =  len(alphabet) + index_with_shift
+            index_with_shift = difference
+        
+        generated_text += alphabet[index_with_shift]
+
+        
+    print(generated_text)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 # direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 # text = input("Type your message:\n").lower()
 # shift = int(input("Type the shift number:\n"))
 
-encrypt("abc", 1)
+encrypt("hello", 3)
+decrypt("khoor", 3)
 
