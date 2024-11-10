9a8be4a48aa7c2d4c6fcbd893978ad3124300c32
Muhammad Abid <abidroid1@gmail.com>
2024-11-10 21:31:44 +0500

    caesar cipher

--- day8/caesar_cipher.py
+++ day8/caesar_cipher.py
@@ -10,7 +10,6 @@ def encrypt(original_text, shift_num):
             index_with_shift = difference
         
         generated_text += alphabet[index_with_shift]
-
         
     print(generated_text)
         
@@ -28,15 +27,32 @@ def decrypt( original_text, shift_num):
         
         generated_text += alphabet[index_with_shift]
 
-        
     print(generated_text)
+
+def caesar(original_text, shift_num, encode_or_decode):
+    if encode_or_decode == "encode":
+        encrypt(original_text, shift_num)
+    else:
+        decrypt(original_text, shift_num)
+
+
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-# text = input("Type your message:\n").lower()
-# shift = int(input("Type the shift number:\n"))
+should_continue = True
 
-encrypt("hello", 3)
-decrypt("khoor", 3)
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar( original_text=text, shift_num=shift, encode_or_decode=direction)
+
+    restart = input("Type 'yes' if want to continue. Else write 'no'\n").lower()
+    if restart == "no":
+        should_continue = False
+        print('Good Bye')
+
+
+
 
