b5ff8081366e4cfc475aa187e62e2cf8664ebb77
Muhammad Abid <abidroid1@gmail.com>
2024-11-13 20:56:05 +0500

    calculator complete

--- day10/calculator_project.py
+++ day10/calculator_project.py
@@ -17,4 +17,26 @@ operations = {
     "/": divide,
 }
 
-print(operations["*"](4, 5))
+def calculator():
+    should_accumulate = True
+
+    num1 = float(input("What is the first number? : "))
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the next number? : "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+
+        if choice == 'y':
+            num1 = answer
+        else:
+            should_accumulate = False
+            print('\n' * 30)
+            calculator()
+
+calculator()
+    
