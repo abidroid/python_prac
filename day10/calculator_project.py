0629c2f417f2b3c2d70e753c04efdaa718d0e7b6
Muhammad Abid <abidroid1@gmail.com>
2024-11-13 20:37:58 +0500

    calculator

---
+++ day10/calculator_project.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    return a+b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+print(operations["*"](4, 5))
