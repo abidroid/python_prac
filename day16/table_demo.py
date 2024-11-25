d50a9d042e63ce0e131a9b5ec2ee3fc64834139f
Muhammad Abid <abidroid1@gmail.com>
2024-11-25 20:50:04 +0500

    fixes

--- day16/table_demo.py
+++ day16/table_demo.py
@@ -1,6 +1,8 @@
 from prettytable import PrettyTable
 
 table = PrettyTable()
+table.add_column('S.No', ["1", "2", "3"])
+
 table.add_column('Framework', ['React Native', 'Flutter', 'Xamarin'])
 table.add_column('Company', ['Meta', 'Google', 'Microsoft'])
 table.add_column('Language', ['Javascript', 'Dart', 'C#'])
