858171450dc73c91d8e94caa53295bcc51746791
Muhammad Abid <abidroid1@gmail.com>
2024-12-25 12:08:18 +0500

    text analyzer

---
+++ text_analyzer/text_analyzer.py
@@ -0,0 +1,43 @@
+class TextAnalyzer(object):
+
+    def __init__(self, text):
+        # remove punctuation
+        formattedText = text.replace('.', '').replace('!', '').replace(',','').replace('?', '')
+
+        # make text lowercase
+        formattedText = formattedText.lower()
+
+        self.fmtText = formattedText
+
+
+    def freqAll(self):
+        # split text into words
+        wordList = self.fmtText.split(' ')
+
+        # create dictionary
+        freqMap = {}
+        for word in set(wordList): # user set to remove duplicates in list
+            freqMap[word] = wordList.count(word)
+
+        return freqMap
+    
+    def freqOf(self, word):
+        # get frequency map
+        freqDict = self.freqAll()
+
+        if word in freqDict:
+            return freqDict[word]
+        else:
+            return 0
+        
+
+givenstring="Lorem ipsum dolor! diam amet, consetetur Lorem magna. sed diam nonumy eirmod tempor. diam et labore? et diam magna. et diam amet."
+analyzed = TextAnalyzer(givenstring)
+print("Formatted Text:", analyzed.fmtText)
+
+freqMap = analyzed.freqAll()
+print(freqMap)
+
+word = "lorem"
+frequency = analyzed.freqOf(word)
+print("The word",word,"appears",frequency,"times.")
